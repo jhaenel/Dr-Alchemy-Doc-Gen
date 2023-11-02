685196ed2bda3c46a34a14436ee06590bbb16e72
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 13:32:13 -0400

    T: get api key

--- tests/AI/test_openai_util.py
+++ tests/AI/test_openai_util.py
@@ -1,4 +1,7 @@
 import pytest
+from src.AI.openai_util import (
+    get_api_key,
+)
 
 # Constants for testing
 TEST_API_KEY = "test-api-key"
@@ -7,4 +10,8 @@ TEST_API_KEY = "test-api-key"
 @pytest.fixture
 def mock_env_vars(monkeypatch):
     monkeypatch.setenv("OPENAI_API_KEY", TEST_API_KEY)
+
+
+def test_get_api_key(mock_env_vars):
+    assert get_api_key() == TEST_API_KEY
 
