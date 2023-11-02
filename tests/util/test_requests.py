8a057dc1001330e448d0d887d820bad20e8917b1
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 14:21:03 -0400

    func that succeceds only on the nth try for testing

--- tests/util/test_requests.py
+++ tests/util/test_requests.py
@@ -2,3 +2,15 @@
 def always_fail():
     raise ValueError("Simulated Failure")
 
+
+def succeeds_on_nth_try(n):
+    call_count = [0]  # Using a list to have a mutable integer
+
+    def _inner():
+        if call_count[0] < n - 1:
+            call_count[0] += 1
+            raise ValueError("Simulated Failure")
+        return "Success"
+
+    return _inner, call_count
+
