b5d7f0fd5d6fd9ddb72f92b117ad83fec5e8eeef
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 13:34:44 -0400

    T: create chat completion body

--- tests/AI/test_openai_util.py
+++ tests/AI/test_openai_util.py
@@ -2,6 +2,7 @@ import pytest
 from src.AI.openai_util import (
     get_api_key,
     create_headers,
+    create_body,
 )
 
 # Constants for testing
@@ -24,4 +25,11 @@ def test_create_headers():
     }
     headers = create_headers(TEST_API_KEY)
     assert headers == expected_headers
+
+
+def test_create_body():
+    user_content = "Hello!"
+    body = create_body(user_content)
+    assert "gpt-3.5-turbo" in body
+    assert "messages" in body
 
