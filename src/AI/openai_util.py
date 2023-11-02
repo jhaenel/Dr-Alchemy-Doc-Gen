c2a1409a00406b02b1f7862ea31c49b54b47245f
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 13:33:42 -0400

    TI: create header with api key and content tpye

--- src/AI/openai_util.py
+++ src/AI/openai_util.py
@@ -1,5 +1,6 @@
 import os
 
+CONTENT_TYPE = "application/json"
 ENV_API_KEY = "OPENAI_API_KEY"
 
 
@@ -8,4 +9,8 @@ def get_api_key():
     if not api_key:
         raise ValueError(f"The '{ENV_API_KEY}' environment variable is not set.")
     return api_key
+
+
+def create_headers(api_key):
+    return {"Content-Type": CONTENT_TYPE, "Authorization": f"Bearer {api_key}"}
 
