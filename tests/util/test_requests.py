2038c27415fcd6a019bf3f6f86cb36ae0abb4734
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 14:25:23 -0400

    TP: exceed max retries

--- tests/util/test_requests.py
+++ tests/util/test_requests.py
@@ -1,4 +1,8 @@
+from unittest.mock import Mock
 from src.util.requests import retry, MAX_RETRIES
+
+import pytest
+
 
 # Function to be decorated for testing purposes
 def always_fail():
@@ -32,4 +36,32 @@ def test_retry_decorator(mocker):
 
     assert call_count[0] == 2  # Function should succeed on the 3rd try
 
+
+
+def test_retry_decorator_exceeds_max_retries(mocker):
+    mock_sleep = mocker.patch("time.sleep")
+    test_func = always_fail
+
+    # Apply the retry decorator
+    decorated_func = retry(
+        exceptions=(ValueError,), tries=MAX_RETRIES, delay=1, backoff=2
+    )(test_func)
+
+    with pytest.raises(ValueError) as exc_info:
+        decorated_func()
+
+    # The exception message should be the one raised by the always_fail function
+    assert "Simulated Failure" in str(exc_info.value)
+
+    # Verify the function was called MAX_RETRIES times
+    # Here we can actually use a Mock to wrap the always_fail function to count calls
+    test_func_mock = Mock(side_effect=always_fail)
+    decorated_func = retry(
+        exceptions=(ValueError,), tries=MAX_RETRIES, delay=1, backoff=2
+    )(test_func_mock)
+
+    with pytest.raises(ValueError):
+        decorated_func()
+
+    assert test_func_mock.call_count == MAX_RETRIES
 
