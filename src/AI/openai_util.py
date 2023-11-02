55821f85bbb165a9ec6652605ef3c726a7251fe7
JustinHaenel <jhaenel10@gmail.com>
2023-11-02 13:35:10 -0400

    TI: simple chat compeltion body with user content arg

--- src/AI/openai_util.py
+++ src/AI/openai_util.py
@@ -1,7 +1,11 @@
 import os
 
+API_MODEL = "gpt-3.5-turbo"
 CONTENT_TYPE = "application/json"
 ENV_API_KEY = "OPENAI_API_KEY"
+SYSTEM_ROLE = "system"
+USER_ROLE = "user"
+SYSTEM_CONTENT = "You are a helpful assistant."
 
 
 def get_api_key():
@@ -13,4 +17,16 @@ def get_api_key():
 
 def create_headers(api_key):
     return {"Content-Type": CONTENT_TYPE, "Authorization": f"Bearer {api_key}"}
+
+
+def create_body(user_content):
+    return json.dumps(
+        {
+            "model": API_MODEL,
+            "messages": [
+                {"role": SYSTEM_ROLE, "content": SYSTEM_CONTENT},
+                {"role": USER_ROLE, "content": user_content},
+            ],
+        }
+    )
 
