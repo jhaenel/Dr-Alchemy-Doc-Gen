019e48ea98e49b7f2a0e21cfc03fc539fa0b0847
jhaenel <99285546+jhaenel@users.noreply.github.com>
2023-11-11 19:34:51 +0000

    Prompt: string hand response format

--- src/AI/generate_comments.py
+++ src/AI/generate_comments.py
@@ -3,6 +3,9 @@ from src.AI.openai_util import generate
 GENERATE_COMENTS_PROMPT = """
 Add any useful, insightful inline comments to the code below.
 Give me back the full unaltered code with your comments added.
+DO NOT ADD ANYTHING OTHER THAN COMMENTS.
+ONLY RESPOND WITH THE CODE AND COMMENTS INA PYTHON CODE.
+Do not put your response inside a code block. And there should be nothing before and nothing after.
 Here is the code:
 
 """
@@ -11,7 +14,6 @@ Here is the code:
 def generate_comments(code_to_comment: str) -> dict:
     comment_prompt = GENERATE_COMENTS_PROMPT + code_to_comment
     return generate(comment_prompt)
-
 
 if __name__ == "__main__":
     code_to_comment = """
