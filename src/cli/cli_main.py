d8eae75b25c3eb6f17f42f77ac3e3daf0e31919d
jhaenel <99285546+jhaenel@users.noreply.github.com>
2023-11-13 17:55:04 -0500

    format and type hint

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -1,15 +1,27 @@
 import click
 from src.orchestration import orchestration
 
+
 @click.command()
-@click.option('-s', '--source', default='.', help='Source directory to copy from. Default is the current directory.')
-@click.option('-d', '--destination', default='doc_gen', help='Destination directory to copy to. Default is "doc_gen".')
-def main(source, destination, model):
+@click.option(
+    "-s",
+    "--source",
+    default=".",
+    help="Source directory to copy from. Default is the current directory.",
+)
+@click.option(
+    "-d",
+    "--destination",
+    default="doc_gen",
+    help='Destination directory to copy to. Default is "doc_gen".',
+)
+def main(source: str, destination: str) -> None:
     """
-    CLI tool to automate generating comments for software, using specified OpenAI model.
+    CLI tool to automate generating comments for software in cloned directory
     """
     click.echo(f"Running with source: {source}, destination: {destination}")
     orchestration(source, destination)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
