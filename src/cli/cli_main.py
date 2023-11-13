fc11be8182364cffd15dace96e14b65eb32091e7
jhaenel <99285546+jhaenel@users.noreply.github.com>
2023-11-13 17:46:19 -0500

    F: include an dpass destination option in cli

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -3,12 +3,13 @@ from src.orchestration import orchestration
 
 @click.command()
 @click.option('-s', '--source', default='.', help='Source directory to copy from. Default is the current directory.')
+@click.option('-d', '--destination', default='doc_gen', help='Destination directory to copy to. Default is "doc_gen".')
 def main(source, destination, model):
     """
     CLI tool to automate generating comments for software, using specified OpenAI model.
     """
-    click.echo(f"Running with source: {source}")
-    orchestration(source,)
+    click.echo(f"Running with source: {source}, destination: {destination}")
+    orchestration(source, destination)
 
 if __name__ == '__main__':
     main()
--- src/orchestration.py
+++ src/orchestration.py
@@ -8,11 +8,11 @@ from src.file_ops.file_operations import (
 )
 
 
-def orchestrate(src:str):
-    copy_all_files(src, "doc_gen")
-    for file in find_all_files_recursively("doc_gen"):
+def orchestrate(src:str, dest:str):
+    copy_all_files(src, dest)
+    for file in find_all_files_recursively(dest):
         replace_file(src=file, new_content=generate_comments(read_file(file)))
-    create_machine_readable_copies("doc_gen", "doc_gen_machine_readable")
+    create_machine_readable_copies(dest, "doc_gen_machine_readable")
 
 
 if __name__ == "__main__":
